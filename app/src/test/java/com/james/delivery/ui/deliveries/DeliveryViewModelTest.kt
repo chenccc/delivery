b617554842aec0a501524cb15e689ce8c0b55b8d
chenccc <1505866443@qq.com>
2021-08-04 16:44:11 +0800

    Add mainCoroutineRule for testing suspend function in viewmodel.

--- app/src/test/java/com/james/delivery/ui/deliveries/DeliveryViewModelTest.kt
+++ app/src/test/java/com/james/delivery/ui/deliveries/DeliveryViewModelTest.kt
@@ -1,12 +1,30 @@
 package com.james.delivery.ui.deliveries
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.james.delivery.MainCoroutineRule
 import com.james.delivery.data.repository.FakeDeliveryRepositoryImpl
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 
+@ExperimentalCoroutinesApi
 class DeliveryViewModelTest {
+
+    @get:Rule
+    var instantRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
     private lateinit var deliveryViewModel: DeliveryViewModel
     @Before
     fun setUp() {
         deliveryViewModel = DeliveryViewModel(FakeDeliveryRepositoryImpl())
+    }
+
+    @Test
+    fun `view model get flow successfully`() {
+
     }
 }
